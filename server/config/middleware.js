d71cc8e542d2577d08e2b0fd11ec43e0b6c0cacb
Clark Feusier <cfeusier@gmail.com>
2015-01-22 15:04:38 -0800

    Finish config/middleware for template serve

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,36 +1,40 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var helpers = require('./helpers.js'); // custom middleware
+var path = require('path');
+var partials = require('express-partials');
+var helpers = require('./helpers.js');
 
 module.exports = function (app, express) {
   // var userRouter = express.Router();
   // var guideRouter = express.Router();
 
-  app.set('views', __dirname + '../templates');
+  // view setup
+  app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
   app.use(partials());
+
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
 
-  // app.use('/api/users', userRouter);
-  // app.use('/api/guides', guideRouter);
-  app.use(helpers.errorLogger);
-  app.use(helpers.errorHandler);
-
-  // serve base client-app payload
+  // serve base-client-app payload
   app.get('/', function(req, res) {
     res.render('index');
   });
 
+  // serve static assets for client-app
+  app.use(express.static(path.join(__dirname, '/../../client')));
+
+  // api routers
+  // app.use('/api/users', userRouter);
+  // app.use('/api/guides', guideRouter);
+
+  // error handlers
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
   // inject routers into respective route files
   // require('../users/userRoutes.js')(userRouter);
   // require('../guides/guideRoutes.js')(guideRouter);
-};
 
-  // MUST BE LAST - catch fallthrough
-  app.get('/*', function(req, res) {
-    res.render('index');
-  });
 };
