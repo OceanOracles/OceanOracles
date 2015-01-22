a5ead9f0a64e85b92a76d61fe78b972ac0ae33d9
Clark Feusier <cfeusier@gmail.com>
2015-01-22 12:06:59 -0800

    Rebase conflict fix in server middleware config

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,6 +6,9 @@ module.exports = function (app, express) {
   // var userRouter = express.Router();
   // var guideRouter = express.Router();
 
+  app.set('views', __dirname + '../templates');
+  app.set('view engine', 'ejs');
+  app.use(partials());
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
@@ -16,7 +19,18 @@ module.exports = function (app, express) {
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
+  // serve base client-app payload
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
+
   // inject routers into respective route files
   // require('../users/userRoutes.js')(userRouter);
   // require('../guides/guideRoutes.js')(guideRouter);
+};
+
+  // MUST BE LAST - catch fallthrough
+  app.get('/*', function(req, res) {
+    res.render('index');
+  });
 };
