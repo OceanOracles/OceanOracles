e0ace105deac804e704067b606fd5a556a0af92e
Eric Kennedy <ekeric13@gmail.com>
2015-01-21 22:44:25 -0800

    Add user model methods to hash password on save and to compare hashed password

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -15,4 +15,26 @@ var UserSchema = new mongoose.Schema({
     update: { type: Date }
 });
 
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 module.exports = mongoose.model('User', UserSchema);
