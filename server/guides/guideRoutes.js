82dc868e05cdd68bb403e8204ec50ac4b77ddc7e
Clark Feusier <cfeusier@gmail.com>
2015-01-23 16:58:26 -0800

    Finish 2 routes in guideRouter

--- server/guides/guideRoutes.js
+++ server/guides/guideRoutes.js
@@ -1,14 +1,14 @@
 var guideController = require('./guideController');
+var auth = require('../users/userController');
 
 module.exports = function(app) {
+  // app.param('guideId', guideController.findGuide);
 
-  // app.param('code', guideController.findGuide)
+  app.route('/')
+    .get(guideController.allGuides)
+    .post(auth.checkAuth, guideController.createGuide);
 
-  // app.route('/')
-  //     .get(guideController.allGuides)
-  //     .post(guideController.newGuide);
-
-  // app.route('/:code')
+  // app.route('/:guideId')
   //     .get(guideController.showGuide)
   //     .put(guideController.editGuide)
   //     .delete(guideController.deleteGuide);
