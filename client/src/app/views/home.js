3bbd12a2a51a12771dd662e480d6180d80ac1e50
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-01-24 05:52:36 +0530

    Add home view and template

---
+++ client/src/app/views/home.js
@@ -0,0 +1,9 @@
+window.HomeView = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    $(this.el).html(this.template());
+    return this;
+  }
+});
