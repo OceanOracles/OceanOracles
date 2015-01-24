385ef14813cbe2a03c6bd2bf3a133ae610572004
Clark Feusier <cfeusier@gmail.com>
2015-01-23 16:58:06 -0800

    Finish allGuides and createGuide routes

--- server/guides/guideController.js
+++ server/guides/guideController.js
@@ -2,15 +2,31 @@ var Guide = require('./guideModel');
 
 module.exports = {
 
-  // newGuide: function(req, res, next){
+  allGuides: function(req, res, next) {
+    Guide.find({}, function(err, guides) {
+      err ? next(err) : res.send(guides);
+    });
+  },
 
-  // },
+  createGuide: function(req, res, next) {
+    var title = req.body.title;
+    var userId = req.user._id;
 
-  // allGuides: function(req, res, next){
+    Guide.findOne({ title: title }, function(err, match) {
+      if (err) {
+        next(err);
+      } else if (match) {
+        res.send(match);
+      } else {
+        var newGuide = { title: title, userId: userId };
+        Guide.create(newGuide, function(err, guide) {
+          err ? next(err) : res.json(guide);
+        });
+      }
+    });
+  },
 
-  // },
-
-  // findGuide: function(req, res, next, code){
+  // findGuide: function(req, res, next, guideId){
 
   // },
 
@@ -26,4 +42,4 @@ module.exports = {
 
   // }
 
-}
+};
