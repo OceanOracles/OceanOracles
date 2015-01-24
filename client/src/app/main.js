43cb3d29b2a408b1cb827550a74e45434c3b8b9c
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-01-24 05:53:29 +0530

    Add new view dependencies

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -1,24 +1,31 @@
-window.Lernhow = {};
-
-Lernhow.Router = Backbone.Router.extend({
+var LernhowRouter = Backbone.Router.extend({
   routes: {
-    '': 'index'
+    '': 'index',
+    'signup': 'signup',
+    '*nF': 'notFound'
   },
   initialize: function() {
-    console.log('Lernhow CLIENT is live ...');
     this.globalNav = new GlobalNavView();
     $('.header').html(this.globalNav.el);
   },
-  index: function(id) {
+  index: function() {
     if (!this.homeView) {
       this.homeView = new HomeView();
     }
     $('#container').html(this.homeView.el);
     // this.globalNav.selectMenuItem('home-menu');
+  },
+  signup: function() {
+    this.signupView = new SignupView();
+    $('#container').html(this.signupView.el)
+  },
+  notFound: function() {
+    this.notFoundView = new NotFoundView();
+    $('#container').html(this.notFoundView.el);
   }
 });
 
-utils.loadTemplates(['GlobalNavView'], function() {
-  new Lernhow.Router();
+utils.loadTemplates(['GlobalNavView', 'HomeView', 'SignupView', 'NotFoundView'], function() {
+  var app = new LernhowRouter();
   Backbone.history.start({ pushState: true });
 });
