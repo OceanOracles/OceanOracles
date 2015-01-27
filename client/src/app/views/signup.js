ad4c8b128e27cecaaa92b75553deef6d5fa591e4
Eric Kennedy <ekeric13@gmail.com>
2015-01-27 14:28:24 -0800

    Add signup view user function to create a user

--- client/src/app/views/signup.js
+++ client/src/app/views/signup.js
@@ -2,8 +2,31 @@ window.SignupView = Backbone.View.extend({
   initialize: function() {
     this.render();
   },
+  events: {
+    'submit' : 'saveUser'
+  },
   render: function() {
     $(this.el).html(this.template());
     return this;
+  },
+  saveUser: function(e){
+    e && e.preventDefault();
+    var $formUsername = this.$el.find('form #username');
+    var $formPassword = this.$el.find('form #password');
+    var $formEmail = this.$el.find('form #email');
+    console.log(e);
+    console.log(this);
+    console.log($formUsername);
+
+    var user = new User({username: $formUsername.val(), password: $formPassword.val(), email: $formEmail.val()});
+    console.log("model object", user);
+    user.save();
+    console.log("model id", user.id);
+    $formUsername.val('');
+    $formPassword.val('');
+    $formEmail.val('');
   }
 });
+
+
+
