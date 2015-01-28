2a946de9f221fd593b1b4991b13aa86592dcfa7f
Eric Kennedy <ekeric13@gmail.com>
2015-01-28 11:57:53 -0800

    Add view for new nav alt template

---
+++ client/src/app/views/globalNavAlt.js
@@ -0,0 +1,9 @@
+window.GlobalNavViewAlt = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    $(this.el).html(this.template());
+    return this;
+  },
+});
