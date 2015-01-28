809dbb7c9d5b6d3ae377eaae494cbc9236ea5b4e
Eric Kennedy <ekeric13@gmail.com>
2015-01-28 15:06:20 -0800

    Refactor signup so that logic is in the user model

--- client/src/app/models/user.js
+++ client/src/app/models/user.js
@@ -9,6 +9,20 @@ window.User = Backbone.Model.extend({
   initialize: function(){
     console.log("A new user object with username " + this.get('username') + "has been created.");
     // this.checkForToken();
+  },
+
+
+
+  signup: function(router){
+    this.save(undefined, {url: "/api/users/signup",
+      success: function(res){
+      window.localStorage.setItem("_token", res.attributes.token);
+      router.navigate('/', { trigger: true })
+    },
+      error: function(){
+        console.log("error when trying to login")
+      }
+    });
   }
 
   // checkForToken: function(){
