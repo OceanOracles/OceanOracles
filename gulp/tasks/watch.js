312d1f8e1fb3a2266946e608c0b9848785243a7e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-01-29 02:36:45 +0530

    Fix gulp watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,5 +4,8 @@ var browserSync = require('browser-sync');
 
 gulp.task('watch', ['browserSync'], function() {
   gulp.watch(config.sass.src, ['sass']);
-  gulp.watch(config.src)
+  gulp.watch(config.js.src, ['appScripts', 'bsReload']);
+  gulp.watch(config.lib.src, ['libScripts', 'bsReload']);
+  gulp.watch(config.templates.src, ['templates', 'bsReload']);
+  gulp.watch(config.html.src, ['html', 'bsReload']);
 });
