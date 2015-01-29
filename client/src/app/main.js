76855bcccea0d612612e84ad97b3ae76a348e1ac
Clark Feusier <cfeusier@gmail.com>
2015-01-29 14:52:06 -0800

    Fix main.js conflict on rebase

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -6,15 +6,26 @@ var LernhowTemplates = [
   'LoginView',
   'GlobalNavViewAuth',
   'GuidePreviewView'
+  'GuideCreateView',
+  'GuideEditView'
 ];
 
 var LernhowRouter = Backbone.Router.extend({
 
   routes: {
+    // static root
     '': 'index',
+
+    // user authentication
     'signup': 'signup',
     'login': 'login',
     'logout': 'logout',
+
+    // guides CRUD
+    'guides/new': 'newGuide',
+    'guides/edit': 'editGuide',
+
+    // client-side catchall
     '*nF': 'notFound'
   },
 
@@ -35,7 +46,7 @@ var LernhowRouter = Backbone.Router.extend({
     appUtils.swapView(this.signupView);
   },
 
-  login: function(){
+  login: function() {
     this.loginView = new LoginView();
     appUtils.swapView(this.loginView);
   },
@@ -44,6 +55,24 @@ var LernhowRouter = Backbone.Router.extend({
     window.localStorage.removeItem("_user.token");
     window.localStorage.removeItem("_user.name");
     this.navigate("/#", { trigger: true });
+  },
+
+  newGuide: function() {
+    if (!appUtils.checkForToken()) {
+      this.navigate('/');
+    } else {
+      this.guideCreateView = new GuideCreateView();
+      appUtils.swapView(this.guideCreateView);
+    }
+  },
+
+  editGuide: function() {
+    if (!appUtils.checkForToken()) {
+      this.navigate('/');
+    } else {
+      this.guideEditView = new GuideEditView();
+      appUtils.swapView(this.guideEditView);
+    }
   },
 
   notFound: function() {
