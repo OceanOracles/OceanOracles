6e0ee37aacae366f2bbadfce1d392d38cc2c4de1
Eric Kennedy <ekeric13@gmail.com>
2015-01-29 16:07:12 -0800

    Add error form view

---
+++ client/src/app/views/formError.js
@@ -0,0 +1,16 @@
+window.formErrorView = Backbone.View.extend({
+
+  template: _.template('<div class="form-error"> <%- text %> </div>'),
+
+  initialize: function(options) {
+    this.html = options;
+    this.render();
+  },
+
+  render: function() {
+    $(this.el).html(this.template(this.html));
+    return this;
+  }
+
+});
+
