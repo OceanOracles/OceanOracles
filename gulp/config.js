f48d892e86e246c91167296b36bd60135d29d22c
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-01 01:49:27 +0530

    Modify config to preserve concat order

--- gulp/config.js
+++ gulp/config.js
@@ -16,7 +16,28 @@ module.exports = {
     watch: ['./server/**/*.js', './client/**/*.js']
   },
   js: {
-    src: src + '/app/**/*.js',
+    src: [
+      src + '/app/auth.js',
+      src + '/app/models/user.js',
+      src + '/app.models/step.js',
+      src + '/app/models/guide.js',
+      src + '/app/collections/guides.js',
+      src + '/app/utils.js',
+      src + '/app/views/globalNav.js',
+      src + '/app/views/globalNavAuth.js',
+      src + '/app/views/guideListItem.js',
+      src + '/app/views/guideList.js',
+      src + '/app/views/guide.js',
+      src + '/app/views/guideAuth.js',
+      src + '/app/views/home.js',
+      src + '/app/views/signup.js',
+      src + '/app/views/formError.js',
+      src + '/app/views/login.js',
+      src + '/app/views/guideCreate.js',
+      src + '/app/views/guideUpdate.js',
+      src + '/app/views/notFound.js',
+      src + '/app/main.js'
+    ],
     dest: dest + '/app'
   },
   templates: {
@@ -24,7 +45,12 @@ module.exports = {
     dest: dest + '/app/templates'
   },
   lib: {
-    src: src + '/lib/**/*.js',
+    src: [
+      src + '/lib/jquery/dist/jquery.js',
+      src + '/lib/underscore/underscore.js',
+      src + '/lib/backbone/backbone.js',
+      src + '/lib/handlebars/handlebars.js'
+    ],
     dest: dest + '/lib'
   },
   html: {
