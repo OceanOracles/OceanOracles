f7910d3cc1549e63364d5700c18e197566df80f2
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-01 01:48:59 +0530

    Add concat and minify functionality to JS build

--- gulp/tasks/appScripts.js
+++ gulp/tasks/appScripts.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var config = require('../config').js;
 
 gulp.task('appScripts', function() {
   return gulp.src(config.src)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(config.dest))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest(config.dest));
 });
--- gulp/tasks/libScripts.js
+++ gulp/tasks/libScripts.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var config = require('../config').lib;
 
 gulp.task('libScripts', function() {
   return gulp.src(config.src)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(config.dest))
+    .pipe(rename('vendor.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest(config.dest));
 });
