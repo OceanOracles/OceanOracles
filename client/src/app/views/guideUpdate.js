ef92d96eac156d1c74e6e38a00843969801940a9
Clark Feusier <cfeusier@gmail.com>
2015-01-30 19:40:12 -0800

    Finish all guide update functionality

--- client/src/app/views/guideUpdate.js
+++ client/src/app/views/guideUpdate.js
@@ -15,7 +15,19 @@ window.GuideUpdateView = Backbone.View.extend({
 
   updateGuide: function(e) {
     e && e.preventDefault();
-    // TODO: get updated fields and update model
+    var newGuideTitle = this.$el.find('#G_title').val();
+    var $stepFields = this.$el.find('.step-input');
+    // update guide on client
+    this.model.set('title', newGuideTitle);
+    // update guide in DB
+    this.model.updateGuide(function() {
+      // update the associated steps in DB
+      var stepsData = appUtils.getStepsData($stepFields, this.model);
+      this.model.updateGuideSteps(stepsData, function() {
+        var guideId = this.model.get('_id');
+        this.router.navigate('/#guides/' + guideId, { trigger: true });
+      }.bind(this));
+    }.bind(this));
   },
 
   deleteGuide: function(e) {
