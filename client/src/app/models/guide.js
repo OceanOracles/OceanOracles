67ad84339bfb9aefb1b85f104ffb5f416a9e125f
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-01 08:46:03 +0530

    Reformat for consistent string quotations

--- client/src/app/models/guide.js
+++ client/src/app/models/guide.js
@@ -3,13 +3,13 @@ window.Guide = Backbone.Model.extend({
   url: '/api/guides',
 
   defaults: {
-    title: ""
+    title: ''
   },
 
   createGuide: function(guideData) {
     this.save(guideData, {
       success: function(model) { model.trigger('created', model); },
-      error: function(model, res) { console.log("error", model, res); }
+      error: function(model, res) { console.log('error', model, res); }
     });
   },
 
