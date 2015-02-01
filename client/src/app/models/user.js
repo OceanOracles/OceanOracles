09ff0c10306f4056fd5ca083eba16b0310919136
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-01 08:52:04 +0530

    Reformat for consistent string quotations

--- client/src/app/models/user.js
+++ client/src/app/models/user.js
@@ -1,49 +1,48 @@
 window.User = Backbone.Model.extend({
 
   defaults: {
-    username: "",
-    password: "",
-    email: ""
+    username: '',
+    password: '',
+    email: ''
   },
 
   login: function(user, router) {
     $.ajax({
-      url: "/api/users/login",
-      type: "POST",
+      url: '/api/users/login',
+      type: 'POST',
       data: user,
       contentType: 'application/json',
       success: function(data) {
-        window.localStorage.setItem("_user.token", data.token);
-        window.localStorage.setItem("_user.Id", data.userId);
+        window.localStorage.setItem('_user.token', data.token);
+        window.localStorage.setItem('_user.Id', data.userId);
         router.navigate('/', { trigger: true });
       },
       error: function(err) {
-        console.log(err.status + " " + err.statusText);
+        console.log(err.status + ' ' + err.statusText);
         var options = {
-          text: "Error: Incorrect username or password. Please try again."
+          text: 'Error: Incorrect username or password. Please try again.'
         };
-        this.formErrorView = new formErrorView(options);
-        $(".error-form-container").html(this.formErrorView.el);
+        this.formErrorView = new FormErrorView(options);
+        $('.error-form-container').html(this.formErrorView.el);
       }
     });
   },
 
   signup: function(router) {
     this.save(undefined, {
-      url: "/api/users/signup",
+      url: '/api/users/signup',
       success: function(data) {
-        window.localStorage.setItem("_user.token", data.attributes.token);
-        window.localStorage.setItem("_user.Id", data.attributes.userId);
+        window.localStorage.setItem('_user.token', data.attributes.token);
+        window.localStorage.setItem('_user.Id', data.attributes.userId);
         router.navigate('/', { trigger: true });
       },
       error: function() {
-        console.log("User signup error");
-        // _this.trigger("signupError", _this);
+        console.log('User signup error');
         var options = {
-          text: "Error: Username or email is already taken. Please try again."
+          text: 'Error: Username or email is already taken. Please try again.'
         };
-        this.formErrorView = new formErrorView(options);
-        $(".error-form-container").html(this.formErrorView.el);
+        this.formErrorView = new FormErrorView(options);
+        $('.error-form-container').html(this.formErrorView.el);
       }
     });
   }
