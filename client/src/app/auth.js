52a22bb6991eeacfc86c128d0d4be3293b741fbc
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-02 06:43:53 +0530

    Add comment explaining backbone sync override

--- client/src/app/auth.js
+++ client/src/app/auth.js
@@ -1,3 +1,7 @@
+/**
+ * Override Backbone's sync to pass the JWT token as part of every request to the server
+ */
+
 var backboneSync = Backbone.sync;
 
 Backbone.sync = function (method, model, options) {
